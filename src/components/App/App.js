1fb7738ffb5136d5de3e9575ee00f430ed00008c
cycho04 <chrisycho04@gmail.com>
2019-05-15 21:31:00 -0700

    added css grid and deck in /utils

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,14 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import UserActions from '../UserActions/UserActions';
 import MainModal from '../MainModal/MainModal';
+import Cards from '../Cards/Cards';
 
 function App() {
   return (
     <div>
       <Header />
       <MainModal />
+      <Cards />
       <UserActions />
       <Footer />
     </div>
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Cards.css';
+
+const Cards = () => {
+    return(
+        <div className='grid-container'>
+            <div className='grid-item'>test</div>
+            <div className='grid-item'>test</div>
+            <div className='grid-item'>test</div>
+            <div className='grid-item'>test</div>
+            <div className='grid-item'>test</div>
+            <div className='grid-item'>test</div>
+            <div className='grid-item'>test</div>
+        </div>
+    )
+}
+
+export default Cards;
